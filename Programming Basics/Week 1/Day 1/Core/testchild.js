e258383d6c627ed45c82896723603e6209ff5811
yamnajouili <yamna.jouili@gmail.com>
2023-10-17 16:08:42 +0200

    commit

---
+++ Programming Basics/Week 1/Day 1/Core/testchild.js
@@ -0,0 +1,23 @@
+var age=15
+var heightchild=55;
+if(heightchild>=42){
+    console.log("Get on that ride, kiddo!");}
+    else {
+        console.log("Sorry kiddo. Maybe next year.");
+    }
+
+    // and
+if(heightchild>=42&&age>=10){
+    console.log("Get on that ride, kiddo!");
+}
+else{
+    console.log("Sorry kiddo. Maybe next year.");
+}
+
+// or
+if(heightchild>=42||age>=10){
+    console.log("Get on that ride, kiddo!");
+}
+else{
+    console.log("Sorry kiddo. Maybe next year.");
+}
