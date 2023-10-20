ef131c1995ac0aedcd06660ba290acdd0d0bb5a7
yamnajouili <yamna.jouili@gmail.com>
2023-10-20 16:37:57 +0200

    commit

---
+++ Programming Basics/Week 1/Day 3/Practice/function_level1,2,3.js
@@ -0,0 +1,11 @@
+function greet(name,hour){
+     if(name=="Count Dooku"){
+        console.log("I'm coming for you, Dooku!" );
+     }
+    else if(name!="Count Dooku") {
+        var  result=console.log("good day,"+name,hour);
+    
+    }
+    return result;
+}
+greet("mohamed","10:00");
