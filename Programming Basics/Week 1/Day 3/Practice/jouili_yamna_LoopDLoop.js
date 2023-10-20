739aa656d2af5d12f937f35bdfbfae6c649b287b
yamnajouili <yamna.jouili@gmail.com>
2023-10-20 12:33:45 +0200

    commit

---
+++ Programming Basics/Week 1/Day 3/Practice/jouili_yamna_LoopDLoop.js
@@ -0,0 +1,26 @@
+//Step 1: 
+// because there are repetitions of steps 
+// the starting of the loop is 0 
+//the loop stops at 6
+//the loop needs knows to stope a condition
+//for each iteration increments tow steps
+// we need a variable i 
+
+for(var i=0;i<7;i++){
+    if(i%2==0 && i!==0 && i!==6){
+        console.log("piece of candy");
+    }
+    if(i==6){
+        console.log("stop giving candy ");
+    }
+}
+
+// ninja
+
+var hour=60
+for(var i=0;i<7;i++){
+    if(i/hour>=5.5 && i%2==0 && i!=6){
+        console.log("piece of candy")
+    }
+    
+}
