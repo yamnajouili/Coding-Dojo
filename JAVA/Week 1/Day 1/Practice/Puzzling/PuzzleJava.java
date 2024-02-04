a2a8cb028f45c5a6f12cc9d7edcc42ffef035221
yamnajouili <yamna.jouili@gmail.com>
2024-02-04 21:21:32 +0100

    code

---
+++ JAVA/Week 1/Day 1/Practice/Puzzling/PuzzleJava.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class PuzzleJava {
+
+    // getTenRolls
+    
+    public ArrayList<Integer> getTenRolls() {
+
+        ArrayList<Integer> random10 = new ArrayList<Integer>();
+        Random rand = new Random();
+
+        for (int i = 1; i <=10; i++) {
+            random10.add(rand.nextInt(20) + 1);
+        }
+
+        return random10;
+    }
+
+
+    // getRandomLetter
+   
+    public String getRandomLetterWithArray() {
+        Random rand = new Random();
+        
+        String alphabetString = "abcdefghijklmnopqrstuvwxyz";
+        String[] alphabet = new String[26];
+        for (int i = 0; i < 26; i++) {
+            alphabet[i] = String.valueOf(alphabetString.charAt(i));
+        }
+        return alphabet[rand.nextInt(26)];
+    }
+    
+    public String getRandomLetter() {
+
+        Random rand = new Random();
+        String alphabetString = "abcdefghijklmnopqrstuvwxyz";
+        char randomChar = alphabetString.charAt(rand.nextInt(26));
+
+        return String.valueOf(randomChar);
+    }
+
+    // generatePassword
+ 
+    public String generatePassword() {
+
+        String password = "";
+
+        for(int i = 0; i < 8; i++) {
+            password = password + getRandomLetter();
+        }
+        return password;
+    }
+
+
+    // getNewPasswordSet
+  .
+    public ArrayList<String> getNewPasswordSet(int length) {
+
+        ArrayList<String> passwordSet = new ArrayList<String>();
+        for(int i = 0; i < length; i++) {
+            passwordSet.add(generatePassword());
+        }
+        return passwordSet;
+
+    }
+
+
+}
---
+++ JAVA/Week 1/Day 1/Practice/Puzzling/Test.java
@@ -0,0 +1,22 @@
+public class Test{
+
+    public static void main (String[] args){
+        PuzzleJava puzzle = new  PuzzleJava();
+    
+    
+    System.out.println(puzzle.getRandomLetter());
+    
+    System.out.println(puzzle. generatePassword());
+    
+    System.out.println(puzzle.getNewPasswordSet());
+    
+    
+    
+    }
+
+
+
+
+
+
+}
